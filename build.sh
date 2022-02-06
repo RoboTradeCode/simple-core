9c9cfe7d6d6a900549c4f17e296c1b4b5e5e5199
nomnoms12 <alexander.ign0918@gmail.com>
2022-02-06 18:49:45 +0300

    Refactoring

--- build.sh
+++ build.sh
@@ -1,27 +1,5 @@
 #!/bin/sh
 
-# Загрузка Boost.Log
-echo "Downloading Boost..."
-cd libs || exit 1
-wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.bz2
-
-# Разархивирование Boost
-echo "Extracting Boost..."
-tar --bzip2 -xf boost_1_78_0.tar.bz2
-rm boost_1_78_0.tar.bz2
-
-# Сборка Boost.Log
-echo "Building Boost.Log..."
-cd boost_1_78_0 || exit 1
-sudo ./bootstrap.sh
-sudo ./b2 install --with-log
-cd ../
-
-# Загрузка Sentry
-echo "Downloading Sentry..."
-git clone --recurse-submodules https://github.com/getsentry/sentry-native.git
-cd ../
-
 # Сборка ядра
 echo "Building Core..."
 mkdir -p build/Debug
