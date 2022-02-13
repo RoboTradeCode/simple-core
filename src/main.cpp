07f36719963cc7348adc0ffef12b1221ceaa22a6
nomnoms12 <alexander.ign0918@gmail.com>
2022-02-13 22:11:15 +0300

    Change build to debug

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,6 @@ int main()
 {
     // Инициализация Sentry
     sentry_options_t* options = sentry_options_new();
-    sentry_options_set_debug(options, true);
     sentry_options_set_dsn(options, SENTRY_DSN);
     sentry_options_set_symbolize_stacktraces(options, true);
     sentry_options_add_attachment(options, CONFIG_FILE_PATH);
