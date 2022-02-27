c5a0eaa5cd04bb95bb6b9181c89c3532b67cc4a6
nomnoms12 <alexander.ign0918@gmail.com>
2022-02-27 16:41:28 +0300

    Improve logging

--- src/logging.cpp
+++ src/logging.cpp
@@ -6,14 +6,41 @@
 void init_logging()
 {
     // Параметры для логирования в файлы
-    int max_size = 1048576 * 5;  // 5 MiB
-    int max_files = 1;
+    int max_size = 1048576 * 2;  // 2 MiB
+    int max_files = 5;
 
-    // Логирование биржевых стаканов, баланса, ошибок и ордеров
-    auto orderbooks = spdlog::rotating_logger_mt<spdlog::async_factory>("orderbooks", "logs/orderbooks.log", max_size, max_files);
-    auto balance = spdlog::rotating_logger_mt<spdlog::async_factory>("balance", "logs/balance.log", max_size, max_files);
-    auto errors = spdlog::rotating_logger_mt<spdlog::async_factory>("errors", "logs/errors.log", max_size, max_files);
-    auto orders = spdlog::rotating_logger_mt<spdlog::async_factory>("orders", "logs/orders.log", max_size, max_files);
+    // Логирование биржевых стаканов
+    auto orderbooks = spdlog::rotating_logger_mt<spdlog::async_factory>(
+        "orderbooks",
+        "logs/orderbooks.log",
+        max_size,
+        max_files
+    );
+
+    // Логирование баланса и ошибок
+    auto balance = spdlog::rotating_logger_mt<spdlog::async_factory>(
+        "balance",
+        "logs/balance.log",
+        max_size,
+        max_files
+    );
+
+    // Логирование ошибок
+    auto errors = spdlog::rotating_logger_mt<spdlog::async_factory>(
+        "errors",
+        "logs/errors.log",
+        max_size,
+        max_files
+    );
+
+    // Логирование ордеров
+    std::vector<spdlog::sink_ptr> orders_sinks;
+    auto orders_file = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/orders.log", max_size, max_files);
+    auto orders_stdout = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    orders_sinks.push_back(orders_file);
+    orders_sinks.push_back(orders_stdout);
+    auto orders = std::make_shared<spdlog::logger>("orders", begin(orders_sinks), end(orders_sinks));
+    spdlog::register_logger(orders);
 
     // Политика сброса буфера
     spdlog::flush_every(std::chrono::seconds(5));
